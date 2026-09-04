340ed58aafd81997990df1c21c8f2f77958f2395
DevBaor <baotranduy666666@gmail.com>
2026-09-04 11:44:10 +0700

    Add wrangler config and worker entry

---
+++ worker.js
@@ -0,0 +1,15 @@
+﻿import html from './index.html';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname === '/screenshot.png') {
+      return fetch('https://raw.githubusercontent.com/DevBaor/BaoTools_1005/gh-pages/screenshot.png');
+    }
+    return new Response(html, {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    });
+  },
+};
